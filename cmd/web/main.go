bdcbc99bf6c52d41d292810d808fff5771211b20
hrmn07 <herman_mac@HRM-MACBOOK.attlocal.net>
2024-03-19 02:00:53 -0500

    added a cli flag feature to choose which port to use. Added a leveled logging features including a more verbose terminal error, http.server error log, all using dependency injection. Finally moving the app routes to routes.go from main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	infoLog  *log.Logger
+	errorLog *log.Logger
+}
+
 func main() {
-	mux := http.NewServeMux()
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
-	FileServer := http.FileServer(http.Dir("./ui/static"))
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	mux.Handle("/static/", http.StripPrefix("/static", FileServer))
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting on server %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
